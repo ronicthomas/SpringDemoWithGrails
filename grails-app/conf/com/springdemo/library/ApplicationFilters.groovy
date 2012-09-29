5223a340cf4aff2bed007b867fcf13ef548df277
Roni C Thomas <roni@intelligrape.com>
2012-09-29 12:48:04 +0530

    Initial commit for spring demo with grails

---
+++ grails-app/conf/com/springdemo/library/ApplicationFilters.groovy
@@ -0,0 +1,18 @@
+package com.springdemo.library
+
+class ApplicationFilters {
+
+    def filters = {
+        all(controller:'*', action:'*') {
+            before = {
+
+            }
+            after = { Map model ->
+
+            }
+            afterView = { Exception e ->
+
+            }
+        }
+    }
+}
---
+++ grails-app/conf/spring/resources.groovy
@@ -0,0 +1,6 @@
+beans = {
+
+    xmlns context: 'http://www.springframework.org/schema/context'
+
+    context.'component-scan'('base-package': 'com.spring3.base.test')
+}
